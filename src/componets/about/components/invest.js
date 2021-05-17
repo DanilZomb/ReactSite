4cd3a60fb60ac21e8e2d4509bc9b62ad9250fa0b
dan <danilsviridenko533@gmail.com>
2021-05-17 14:14:31 +0300

    updated criterias

--- src/componets/about/components/invest.js
+++ src/componets/about/components/invest.js
@@ -6,18 +6,26 @@ function Invest() {
   return (
     <section className="invest">
       <div className="invest-wrapper">
-        <h2 className="invest-title">
-          Invest property for better <span class="text-color">business</span>
-        </h2>
-        <p className="invest-desc">
-          We are committed to processing the information in order to contact you
-          and talk about your project. We are committed to processing the
-          information.
-        </p>
-        <button className="invest-btn">Learn more</button>
-      </div>
-      <div className="invest-img-wrapper">
-        <img src={BusinessPrice} alt="business-price" className="investImg" width="403" height="414"/>
+        <div className="invest-text-wrapper">
+          <h2 className="invest-title">
+            Invest property for better <span class="text-color">business</span>
+          </h2>
+          <p className="invest-desc">
+            We are committed to processing the information in order to contact
+            you and talk about your project. We are committed to processing the
+            information.
+          </p>
+          <button className="invest-btn">Learn more</button>
+        </div>
+        <div className="invest-img-wrapper">
+          <img
+            src={BusinessPrice}
+            alt="business-price"
+            className="investImg"
+            width="403"
+            height="414"
+          />
+        </div>
       </div>
     </section>
   );
